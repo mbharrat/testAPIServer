351cd4c182d85710666d135cd7d6bdf95ae3bf37
mbharratmomentum <mbharrat@momentumsolar.com>
2019-09-17 15:42:21 -0400

    added all route and updated https string to http

--- index.js
+++ index.js
@@ -1,21 +1,32 @@
-const data = require("./data/polist.js")
-const cors = require('cors');
-const express = require('express')
-const app = express()
-const port = 8080
+const data = require("./data/polist.js");
+const cors = require("cors");
+const express = require("express");
+const app = express();
+const port = 8080;
 app.use(cors());
 
-app.get('/', (req, res) => res.send('Your temporary test server is up and running'))
+app.get("/", (req, res) =>
+  res.send("Your temporary test server is up and running")
+);
 
 app.get("/prodmh/getpolist", (req, res) => {
-		console.log(data)
-		//headers to prevent CORS
-		res.header("Access-Control-Allow_Origin", "*")
-		res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
-		//
-		res.json(data.polist)
-})
-	
+  //   console.log(data);
+  //headers to prevent CORS
+  const test = "hi";
+  res.header("Access-Control-Allow_Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
 
+  res.json(data.polist);
+});
 
-app.listen(port, () => console.log(`Test server is running on at \nhttps://localhost:${port}/`))
+app.get("/all", (req, res) => {
+  const routes = ["/prodmh/getpolist"];
+  res.json(routes);
+});
+
+app.listen(port, () =>
+  console.log(`Test server is running on at \nhttp://localhost:${port}/`)
+);
