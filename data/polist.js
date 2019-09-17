b6cc0dc6378e9023f54e89dfa5784b68351e6793
Michael Bharrat <mbharrat@gmail.com>
2019-09-17 15:10:16 -0400

    header changes

---
+++ data/polist.js
@@ -0,0 +1,312 @@
+const polist = [
+    {
+        "Name": "TRU-DEFINITION DURATION AR \n3 BDL. PER. SQ.",
+        "Colors": [],
+        "Package": "SQ.",
+        "Type": "Product",
+        "itemid": 1001
+    },
+    {
+        "Name": "DURARIDGE \n20FT PER BDL.",
+        "Colors": [],
+        "Package": "BDL",
+        "Type": "Product",
+        "itemid": 1002
+    },
+    {
+        "Name": "WEATHERLOCK MAT \n2 SQ. Roll - 66 Lin. ft/ROLL X 3FT",
+        "Colors": [],
+        "Package": "ROLL",
+        "Type": "Product",
+        "itemid": 1003
+    },
+    {
+        "Name": "UNDERLAYMENT DECK DEFENSE \n10 SQ PER RL 250 FT X 4 FT",
+        "Colors": [],
+        "Package": "ROLL",
+        "Type": "Product",
+        "itemid": 1004
+    },
+    {
+        "Name": "DRIP EDGE \nF 5.5 / F8.5 / C4.5 - 10FT. / PC",
+        "Colors": [
+            "WHITE",
+            "BROWN"
+        ],
+        "Options": [
+            "F5.5",
+            "F8.5",
+            " C4.5"
+        ],
+        "Package": "ROLL",
+        "Type": "Product",
+        "itemid": 1005
+    },
+    {
+        "Name": "STARTER STRIP \n 105 Lin ft/Bndl",
+        "Colors": [],
+        "Package": "BDL",
+        "Type": "Product",
+        "itemid": 1006
+    },
+    {
+        "Name": "VENTSURE 4 / EXHAUST \n 4FT PCS. / 10 PER BOX",
+        "Colors": [],
+        "Package": "BDL",
+        "Type": "Product",
+        "itemid": 1007
+    },
+    {
+        "Name": "SLANT BACK VENTS ",
+        "Colors": [
+            "BLACK",
+            "WHITE",
+            "GRAY",
+            "BROWN"
+        ],
+        "Package": "EA",
+        "Type": "Product",
+        "itemid": 1008
+    },
+    {
+        "Name": "PIPE BOOT 1' to 3\"",
+        "Colors": [],
+        "Package": "EA",
+        "Type": "Product",
+        "itemid": 1009
+    },
+    {
+        "Name": "PIPE BOOT 3\" to 4\"",
+        "Colors": [],
+        "Package": "EA",
+        "Type": "Product",
+        "itemid": 1010
+    },
+    {
+        "Name": "ROOF NAILS COIL 1 1/2 /n COIL UP TO 15SQ ",
+        "Colors": [],
+        "Package": "BOX",
+        "Type": "Product",
+        "itemid": 1011
+    },
+    {
+        "Name": "ROOF NAILS COIL 1 1/4 \n COIL UP TO 15SQ ",
+        "Colors": [],
+        "Package": "BOX",
+        "Type": "Product",
+        "itemid": 1012
+    },
+    {
+        "Name": "CAP NAILS BOX \n 25 SQs per BOX",
+        "Colors": [],
+        "Package": "BOX",
+        "Type": "Product",
+        "itemid": 1013
+    },
+    {
+        "Name": "STAPLES \n T10 / 3/8\"",
+        "Colors": [],
+        "Package": "BOX",
+        "Type": "Product",
+        "itemid": 1014
+    },
+    {
+        "Name": "DECK NAILS COIL \n 2.5\" 8 D",
+        "Colors": [],
+        "Package": "BOX",
+        "Type": "Product",
+        "itemid": 1015
+    },
+    {
+        "Name": "DECK NAILS COIL \n 3.10\" 10 D",
+        "Colors": [],
+        "Package": "BOX",
+        "Type": "Product",
+        "itemid": 1016
+    },
+    {
+        "Name": "STEP FLASHING 25FT. PER",
+        "Colors": [],
+        "Package": "BDL",
+        "Type": "Product",
+        "itemid": 1017
+    },
+    {
+        "Name": "SKYLIGHT MEASUREMENT",
+        "Colors": [],
+        "Package": "BDL",
+        "Type": "Measure",
+        "Measures": [
+            "Inside LxW",
+            "Roof LxW"
+        ],
+        "itemid": 1018
+    },
+    {
+        "Name": "FLASHING KIT",
+        "Colors": [],
+        "Package": "EA",
+        "Type": "Question",
+        "itemid": 1019
+    },
+    {
+        "Name": "TRIM NAILS 1 1/4",
+        "Colors": [],
+        "Package": "BOX",
+        "Type": "Product",
+        "itemid": 1021
+    },
+    {
+        "Name": "KP I-SPAN D5 SOFFIT 12\" EA.",
+        "Colors": [],
+        "Package": "PCS",
+        "Type": "Product",
+        "itemid": 1022
+    },
+    {
+        "Name": "KP 3/4 J-CHANEL",
+        "Colors": [],
+        "Package": "PCS",
+        "Type": "Product",
+        "itemid": 1023
+    },
+    {
+        "Name": "GENTEK SMOOTH COIL \n 2'X50' RL",
+        "Colors": [],
+        "Package": "RLS",
+        "Type": "Product",
+        "itemid": 1024
+    },
+    {
+        "Name": "STEP FLASHING 25FT PER",
+        "Colors": [],
+        "Package": "BDL",
+        "Type": "Product",
+        "itemid": 1025
+    },
+    {
+        "Name": "FACIA BOARD 1/8",
+        "Colors": [],
+        "Package": "LIN FT",
+        "Type": "Product",
+        "itemid": 1026
+    },
+    {
+        "Name": "FACIA BOARD 1/6",
+        "Colors": [],
+        "Package": "LIN FT",
+        "Type": "Product",
+        "itemid": 1027
+    },
+    {
+        "Name": "TRIMNAILS 1 1.4",
+        "Colors": [],
+        "Package": "BOX",
+        "Type": "Product",
+        "itemid": 1028
+    },
+    {
+        "Name": "KP I-SPAN D5 SOFFIT  12\" EA",
+        "Colors": [],
+        "Package": "PCS",
+        "Type": "Product",
+        "itemid": 1029
+    },
+    {
+        "Name": "KP 3/4'",
+        "Colors": [],
+        "Package": "PCS",
+        "Type": "Product",
+        "itemid": 1030
+    },
+    {
+        "Name": "GENTEK SMOOTH COIL \n 2'X50' RL",
+        "Colors": [],
+        "Package": "RLS",
+        "Type": "Product",
+        "itemid": 1031
+    },
+    {
+        "Name": "GUTTERS LIN FT",
+        "Colors": [],
+        "Type": "Measure",
+        "Measures": [
+            "Lin FT"
+        ],
+        "itemid": 1032
+    },
+    {
+        "Name": "DOWNSPOUT LF",
+        "Colors": [],
+        "Type": "Measure",
+        "Measures": [
+            "Lin FT"
+        ],
+        "itemid": 1033
+    },
+    {
+        "Name": "LOW SLOPE EPDM SQRS",
+        "Colors": [],
+        "Type": "Measure",
+        "Measures": [
+            "SQRS"
+        ],
+        "itemid": 1034
+    },
+    {
+        "Name": "ROOF PITCH WALKABLE SQRS",
+        "Colors": [],
+        "Type": "Measure",
+        "Measures": [
+            "SQRS"
+        ],
+        "itemid": 1035
+    },
+    {
+        "Name": "ROOF PITCH NON-WALKABLE SQRS",
+        "Colors": [],
+        "Type": "Measure",
+        "Measures": [
+            "SQRS"
+        ],
+        "itemid": 1036
+    },
+    {
+        "Name": "# OF LAYERS",
+        "Colors": [],
+        "Type": "Measure",
+        "Measures": [
+            "Count"
+        ],
+        "itemid": 1037
+    },
+    {
+        "Name": "CEDAR SHAKE",
+        "Colors": [],
+        "Type": "Product",
+        "itemid": 1038
+    },
+    {
+        "Name": "# OF CHIMMNEYS",
+        "Colors": [],
+        "Type": "Product",
+        "itemid": 1039
+    },
+    {
+        "Name": "PRE-EXISTING CONDITIONS",
+        "Colors": [],
+        "Type": "Notes",
+        "itemid": 1040
+    },
+    {
+        "Name": "NOTES",
+        "Colors": [],
+        "Package": "",
+        "Type": "Notes",
+        "itemid": 1041
+    }
+]
+
+module.exports = {
+	polist
+}
